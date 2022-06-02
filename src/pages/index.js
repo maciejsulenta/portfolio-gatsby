2a99a4851f54a4331469767fd55725f56792c15c
dekzzie <perecadrian@gmail.com>
2022-06-02 23:09:44 +0200

    main : add heading to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "../styles/style.scss";
+import "../styles/home.scss";
 import Header from "../components/Header";
 import CursorManager from "../components/CustomCursor/CursorManager";
 import CustomCursor from "../components/CustomCursor";
 
 const Index = () => {
   return (
-    <>
-      {/* <CustomCursor /> */}
+    <CursorManager>
+      <CustomCursor />
       <Header />
-    </>
+      <div className="home">
+        <h1 className="home__name">PEES</h1>
+      </div>
+    </CursorManager>
   );
 };
 export default Index;
